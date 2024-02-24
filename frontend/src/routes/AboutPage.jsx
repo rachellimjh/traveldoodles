dbc759aec87de0f42c96e4a0a317b499fefd14d7
Rachel Lim <rachellimjh@gmail.com>
2024-02-25 05:04:07 +0800

    fndfnjkb s

---
+++ frontend/src/routes/AboutPage.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import NavBar from "../components/NavBar";
+import "../routes/css/AboutPage.css";
+
+export default function AboutPage() {
+  return (
+    <>
+      <NavBar />
+      <h1>About TravelDoodles</h1>
+      <p className="description">
+        TravelDoodles allows users to freeze their most cherished travel moments
+        in time by featuring functionalities such as posting travel blogs and
+        pining recent travel locations.
+      </p>
+      <h2>Different Tags:</h2>
+      <p>
+        JungleDoodles: For adventures
+        <br />
+        TruffleDoodles: For new food finds
+        <br />
+        SnoozerDoodles: For accommodation recommendations
+        <br />
+        WonderDoodles: For sightseeing spots
+        <br />
+        StudyDoodles: For study spots recommendations
+        <br />
+        FitDoodles: For outfits
+      </p>
+    </>
+  );
+}
